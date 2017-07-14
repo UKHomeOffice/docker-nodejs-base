d41eca669ff00ffa4f58a6feb1d2ab0d6677d646
Ben Marvell <madmarv187@gmail.com>
2017-07-14 13:19:11 +0100

    Add drone build.
    Publish a v{major}.{minor}.{patch} version of node based on the `NODE_VERSION` in the dockerfile
    
    Publish a v{major}.{minor}
    
    Publish a v{major}
    
    This allows you to pick and choose how you'd like to manage your node versions and gives us a slack version of semver in our dockerfiles (rebuild the downstream image when you need a new version of 6 for example).

---
+++ bin/image_to_quay.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#get node version from dockerfile
+NODE_VERSION=$(grep "ENV NODE_VERSION" Dockerfile | sed 's/^ENV NODE_VERSION v//');
+
+#get major, minor and patch
+MAJOR=`echo $NODE_VERSION | awk '{split($0,a,"."); print a[1]}'`;
+MINOR=`echo $NODE_VERSION | awk '{split($0,a,"."); print a[2]}'`;
+PATCH=`echo $NODE_VERSION | awk '{split($0,a,"."); print a[3]}'`;
+
+docker login -u="ukhomeofficedigital+drone" -p=${DOCKER_PASSWORD} quay.io;
+
+#major, minor patch version
+echo "Publishing v${MAJOR}.${MINOR}.${PATCH} of nodejs-base...";
+docker tag firearms quay.io/ukhomeofficedigital/nodejs-base:v${MAJOR}.${MINOR}.${PATCH}
+docker push quay.io/ukhomeofficedigital/nodejs-base:v${MAJOR}.${MINOR}.${PATCH}
+echo "published v${MAJOR}.${MINOR}.${PATCH}";
+
+#major, minor version
+echo "Publishing v${MAJOR}.${MINOR} of nodejs-base";
+docker tag firearms quay.io/ukhomeofficedigital/nodejs-base:v${MAJOR}.${MINOR}
+docker push quay.io/ukhomeofficedigital/nodejs-base:v${MAJOR}.${MINOR}
+echo "published v${MAJOR}.${MINOR}";
+
+#major
+echo "Publishing v${MAJOR} of nodejs-base";
+docker tag firearms quay.io/ukhomeofficedigital/nodejs-base:v${MAJOR};
+docker push quay.io/ukhomeofficedigital/nodejs-base:v${MAJOR};
+echo "published v${MAJOR}";
