e5706e292fa6d948c691e9e761c4b09a3bb7770f
Robin Knipe <robin.knipe@gmail.com>
2017-07-26 14:27:36 +0100

    Create common function `tag_n_push` for simplicity

--- bin/image_to_quay.sh
+++ bin/image_to_quay.sh
@@ -1,29 +1,27 @@
 #!/bin/sh
 
 #get node version from dockerfile
-NODE_VERSION=$(grep "ENV NODE_VERSION" Dockerfile | sed 's/^ENV NODE_VERSION v//');
+NODE_VERSION=$(grep "ENV NODE_VERSION" Dockerfile | sed 's/^ENV NODE_VERSION v//')
 
 #get major, minor and patch
-MAJOR=`echo $NODE_VERSION | awk '{split($0,a,"."); print a[1]}'`;
-MINOR=`echo $NODE_VERSION | awk '{split($0,a,"."); print a[2]}'`;
-PATCH=`echo $NODE_VERSION | awk '{split($0,a,"."); print a[3]}'`;
+MAJOR=`echo $NODE_VERSION | awk '{split($0,a,"."); print a[1]}'`
+MINOR=`echo $NODE_VERSION | awk '{split($0,a,"."); print a[2]}'`
+PATCH=`echo $NODE_VERSION | awk '{split($0,a,"."); print a[3]}'`
 
-docker login -u="ukhomeofficedigital+drone" -p=${DOCKER_PASSWORD} quay.io;
+docker login -u="ukhomeofficedigital+drone" -p="${DOCKER_PASSWORD}" quay.io
+
+tag_n_push() {
+  echo "Publishing v${1} of nodejs-base..."
+  docker tag nodejs-base "quay.io/ukhomeofficedigital/nodejs-base:v${1}"
+  docker push "quay.io/ukhomeofficedigital/nodejs-base:v${1}"
+  echo "published v${1}"
+}
 
 #major, minor patch version
-echo "Publishing v${MAJOR}.${MINOR}.${PATCH} of nodejs-base...";
-docker tag nodejs-base quay.io/ukhomeofficedigital/nodejs-base:v${MAJOR}.${MINOR}.${PATCH}
-docker push quay.io/ukhomeofficedigital/nodejs-base:v${MAJOR}.${MINOR}.${PATCH}
-echo "published v${MAJOR}.${MINOR}.${PATCH}";
+tag_n_push "${MAJOR}.${MINOR}.${PATCH}"
 
 #major, minor version
-echo "Publishing v${MAJOR}.${MINOR} of nodejs-base";
-docker tag nodejs-base quay.io/ukhomeofficedigital/nodejs-base:v${MAJOR}.${MINOR}
-docker push quay.io/ukhomeofficedigital/nodejs-base:v${MAJOR}.${MINOR}
-echo "published v${MAJOR}.${MINOR}";
+tag_n_push "${MAJOR}.${MINOR}"
 
 #major
-echo "Publishing v${MAJOR} of nodejs-base";
-docker tag nodejs-base quay.io/ukhomeofficedigital/nodejs-base:v${MAJOR};
-docker push quay.io/ukhomeofficedigital/nodejs-base:v${MAJOR};
-echo "published v${MAJOR}";
+tag_n_push "${MAJOR}"
